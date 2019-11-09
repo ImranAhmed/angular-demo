563a58abb801f7e6df1eb4522d0484cb6002d6fc
Imran Ahmed <imran.ahmed@qbridgeplatform.com>
2019-11-09 14:53:06 +0000

    Added unit test to check img in table

--- react-demo/src/components/UserTable.test.js
+++ react-demo/src/components/UserTable.test.js
@@ -8,8 +8,7 @@ describe('user table', () => {
         {
             "id": 1,
             "email": "george.bluth@reqres.in",
-            "first_name":
-                "George",
+            "first_name": "George",
             "last_name": "Bluth",
             "avatar": "https://image"
         }
@@ -27,7 +26,7 @@ describe('user table', () => {
 
     it('should contain name table header', () => {
         const wrapper = mount(<UserTable data={{ users }} />);
-        expect(wrapper.find('table > thead > tr > th').first(1)).toHaveText('Name');
+        expect(wrapper.find('table > thead > tr > th').first()).toHaveText('Name');
     });
 
     it('should contain email table header', () => {
@@ -43,5 +42,30 @@ describe('user table', () => {
     it('should contain actions table header', () => {
         const wrapper = mount(<UserTable data={{ users }} />);
         expect(wrapper.find('table > thead > tr > th').at(3)).toHaveText('Actions');
+    });
+
+    it('should contain table rows', () => {
+        const wrapper = mount(<UserTable data={{ users }} />);
+        expect(wrapper.find('table > tbody > tr').length).toBe(1);
+    });
+
+    it('should contain table row items', () => {
+        const wrapper = mount(<UserTable data={{ users }} />);
+        expect(wrapper.find('table > tbody > tr > td').length).toBe(4);
+    });
+
+    it('should contain table row with name', () => {
+        const wrapper = mount(<UserTable data={{ users }} />);
+        expect(wrapper.find('table > tbody > tr > td').first()).toHaveText('George Bluth');
+    });
+
+    it('should contain table row with email', () => {
+        const wrapper = mount(<UserTable data={{ users }} />);
+        expect(wrapper.find('table > tbody > tr > td').at(1)).toHaveText('george.bluth@reqres.in');
+    });
+
+    it('should contain table row with avatar', () => {
+        const wrapper = mount(<UserTable data={{ users }} />);
+        expect(wrapper.find('table > tbody > tr > td > img.avatar[src="https://image"]')).toExist();
     });
 });
