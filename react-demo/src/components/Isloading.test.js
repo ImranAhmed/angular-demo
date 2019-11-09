0867989fe6c45a2d4edeca7c8b5eb66243cd9cb0
Imran Ahmed <imran.ahmed@qbridgeplatform.com>
2019-11-09 18:59:46 +0000

    Added unit test to check role attribute on IsLoading component

--- react-demo/src/components/Isloading.test.js
+++ react-demo/src/components/Isloading.test.js
@@ -4,9 +4,19 @@ import React from 'react';
 import IsLoading from './IsLoading';
 
 describe('IsLoading', () => {
-    it('should contain spinner', () => {
+
+    it('should contain a single div', () => {
+        const wrapper = mount(<IsLoading />);
+        expect(wrapper.find('div').length).toBe(1);
+    });
+
+    it('should contain div with spinner class', () => {
         const wrapper = mount(<IsLoading />);
         expect(wrapper.find('div').first()).toMatchSelector('.spinner-border');
     });
 
+    it('should contain div with role attribute', () => {
+        const wrapper = mount(<IsLoading />);
+        expect(wrapper.find('div[role="status"]')).toExist();;
+    });
 });
